290bb4f244da41b4d85c6e5d9814e11d8541260c
ako-tech <akotech20@gmail.com>
2023-02-05 21:37:20 +0100

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   deliveryForm = new FormGroup({
     deliveryOption: new FormControl('shipping'),
     address: new FormControl(''),
   });
 
   get deliveryOption() {
-    return this.deliveryForm.controls['deliveryOption'];
+    return this.deliveryForm.controls.deliveryOption;
+  }
+  get address() {
+    return this.deliveryForm.controls.address;
+  }
+
+  subcription!: Subscription;
+
+  ngOnInit(): void {
+    this.subcription = this.deliveryOption.valueChanges.subscribe(
+      (deliveryOption) => this.disableAddress(deliveryOption === 'pickup')
+    );
+  }
+
+  disableAddress(condition: boolean) {
+    condition ? this.address.disable() : this.address.enable();
   }
 }
