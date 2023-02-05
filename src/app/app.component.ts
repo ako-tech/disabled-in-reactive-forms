e61fd9484c1f1603c55cb127fffc9a99980703f4
ako-tech <akotech20@gmail.com>
2023-02-05 21:30:10 +0100

    base app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  deliveryForm = new FormGroup({
+    deliveryOption: new FormControl('shipping'),
+    address: new FormControl(''),
+  });
+
+  get deliveryOption() {
+    return this.deliveryForm.controls['deliveryOption'];
+  }
+}
